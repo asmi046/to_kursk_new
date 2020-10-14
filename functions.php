9e09bab8b5440ae4ed6c02a5849d227c608a3a6c
asmi046 <asmi046@gmail.com>
2020-10-15 02:47:29 +0300

    All files load

---
+++ functions.php
@@ -0,0 +1,137 @@
+<?php
+
+define("COMPANY_NAME", "<Вписываем имя компании>");
+define("MAIL_RESEND", "<Вписываем почтовый ящик который подставляется в шапку письма>");
+
+
+//----Подключене carbon fields
+//----Инструкции по подключению полей см. в комментариях themes-fields.php
+include "carbon-fields/carbon-fields-plugin.php";
+
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
+
+add_action( 'carbon_fields_register_fields', 'crb_attach_theme_options' ); 
+function crb_attach_theme_options() {
+	require_once __DIR__ . "/themes-fields.php";
+}
+
+add_action( 'after_setup_theme', 'crb_load' );
+function crb_load() {
+	require_once( 'carbon-fields/vendor/autoload.php' );
+	\Carbon_Fields\Carbon_Fields::boot();
+}
+
+//-----Блок описания вывода меню
+// 1. Осмысленные названия для алиаса и для описания на русском.
+// если это меню в подвали пишем - Меню в подвале 
+// если в шапке то пишем - Меню в шапке 
+// если 2 меню в шапке пишем  - Меню в шапке (верхняя часть)
+
+register_nav_menus( array(
+	'header_menu' => 'Главное меню'
+) );
+
+add_theme_support( 'post-thumbnails' );
+set_post_thumbnail_size( 185, 185 ); 
+
+add_post_type_support( 'page', 'excerpt' );
+
+// Подключение стилей и nonce для Ajax в админку 
+add_action('admin_head', 'my_assets_admin');
+function my_assets_admin(){
+	wp_enqueue_style("style-adm",get_template_directory_uri()."/style-admin.css");
+	
+	wp_localize_script( 'jquery', 'allAjax', array(
+			'nonce'   => wp_create_nonce( 'NEHERTUTLAZIT' )
+		) );
+}
+
+// Подключение стилей и nonce для Ajax и скриптов во фронтенд 
+
+define("STYLEVERSION", "1.0.1");
+define("SCRIPTVERSION", "1.0.1");
+
+add_action( 'wp_enqueue_scripts', 'my_assets' );
+	function my_assets() {
+
+		// Подключение стилей 
+
+		wp_enqueue_style("style-modal", get_template_directory_uri()."/css/jquery.arcticmodal-0.3.css", array(), STYLEVERSION, 'all'); //Модальные окна (стили)
+		wp_enqueue_style("style-lightbox", get_template_directory_uri()."/css/lightbox.min.js", array(), STYLEVERSION, 'all'); //Лайтбокс (стили)
+		wp_enqueue_style("style-slik", get_template_directory_uri()."/css/slick.css", array(), STYLEVERSION, 'all'); //Слайдер (стили)
+
+		wp_enqueue_style("main-style", get_stylesheet_uri(), array(), STYLEVERSION, 'all' ); // Подключение основных стилей в самом конце
+
+		// Подключение скриптов
+		
+		wp_enqueue_script( 'jquery');
+
+		wp_enqueue_script( 'amodal', get_template_directory_uri().'/js/jquery.arcticmodal-0.3.min.js', array(), SCRIPTVERSION , true); //Модальные окна
+		wp_enqueue_script( 'mask', get_template_directory_uri().'/js/jquery.inputmask.bundle.js', array(), SCRIPTVERSION , true); //маска для инпутов
+		wp_enqueue_script( 'lightbox', get_template_directory_uri().'/js/lightbox.min.js', array(), SCRIPTVERSION , true); //Лайтбокс
+		wp_enqueue_script( 'slick', get_template_directory_uri().'/js/slick.min.js', array(), SCRIPTVERSION , true); //Слайдер
+
+		wp_enqueue_script( 'main', get_template_directory_uri().'/js/main.js', array(), SCRIPTVERSION , true); // Подключение основного скрипта в самом конце
+		
+		
+		wp_localize_script( 'main', 'allAjax', array(
+			'ajaxurl' => admin_url( 'admin-ajax.php' ),
+			'nonce'   => wp_create_nonce( 'NEHERTUTLAZIT' )
+		) );
+	}
+
+	// Заготовка для вызова ajax
+	
+	add_action( 'wp_ajax_aj_fnc', 'aj_fnc' );
+	add_action( 'wp_ajax_nopriv_aj_fnc', 'aj_fnc' );
+
+	function aj_fnc() {
+		if ( empty( $_REQUEST['nonce'] ) ) {
+			wp_die( '0' );
+		}
+		
+		if ( check_ajax_referer( 'NEHERTUTLAZIT', 'nonce', false ) ) {
+
+
+			
+		} else {
+			wp_die( 'НО-НО-НО!', '', 403 );
+		}
+	}
+	
+	
+	/* Отправка почты
+		
+			$headers = array(
+				'From: Сайт '.COMPANY_NAME.' <MAIL_RESEND>',
+				'content-type: text/html',
+			);
+		
+			add_filter('wp_mail_content_type', create_function('', 'return "text/html";'));
+			
+			$adr_to_send = <Присваиваем поле карбона c адресами для отправки>;
+			$mail_content = "<Тело письма>";
+			$mail_them = "<Тема письма>";
+
+			if (wp_mail($adr_to_send, $mail_them, $mail_content, $headers)) {
+				wp_die(json_encode(array("send" => true )));
+			}
+			else {
+				wp_die( 'Ошибка отправки!', '', 403 );
+			}
+	*/
+	
+	
+	/*	Заготовка шорткода
+		function true_url_external( $atts ) {
+			$params = shortcode_atts( array( // в массиве укажите значения параметров по умолчанию
+				'anchor' => 'Миша Рудрастых', // параметр 1
+				'url' => 'https://misha.blog', // параметр 2
+			), $atts );
+			return "<a href='{$params['url']}' target='_blank'>{$params['anchor']}</a>";
+		}
+		add_shortcode( 'trueurl', 'true_url_external' );
+	*/
+	
+?>
