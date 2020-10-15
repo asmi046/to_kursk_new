dc40df924f18e818a4ea92cb53e0a5123eb34b2c
ruvick <rudikov.web@ya.ru>
2020-10-15 16:47:33 +0300

    Добавил новую страницы

---
+++ template-parts/useful-info.php
@@ -0,0 +1,220 @@
+<section class="useful-info">
+	<div class="inner2">
+		<h2 class="big-title">Полезная информация</h2>
+		<div class="accordion">
+			<div class="accordion__head">
+				Порядок действий при аварии
+			</div>
+			<div class="accordion__body">
+				<p>
+					Действия, которые необходимо предпринять для получения выплат по электронному ОСАГО:
+				</p>
+				<p>
+					1. Заполнить извещение о ДТП с подписью сотрудника ГИБДД и номером его нагрудного знака. Это делается в том случае, если кто-то претендует на возмещение ущерба.Водители обязаны сообщить друг другу сведения о своём страховом полисе ОСАГО: номер, число, наименование, адрес и телефонзногласиотносительно обстоятельств ДТП его участники указывают в нём же. При аварии более двух автомобилей и разногласиях участников в оценке случившегося, а также невозможности совместного заполнения одного бланка Извещения (например, по состоянию здоровья или в случае гибели, в связи с отказом одного из участников от совместного заполнения бланка по иным причинам) каждый водитель вправе заполнить только свой бланк, указав причину.
+				</p>
+				<p>
+					2. Известить страховую компанию или её представителя при первой возможности. Это можно сделать, придя лично с извещением о ДТП. Можно не приходить, а отправить извещение о ДТП в течение первых трех дней со дня аварии любым способом, обеспечивающим подтверждение отправки, страховой компании или её представителю (в субъекте РФ по месту жительства (нахождения) потерпевшего либо в субъекте РФ, на территории которого произошло ДТП).
+				</p>
+				<p class="hide">
+					3. Подать в течение трёх дней заявление о страховой выплате и все необходимые для этого документы. Если ДТП произошло в отдалении от страховой компании или её представителей, есть объективные трудности с подачей заявления и подготовкой всех документов, допускается отсрочка до шести дней.
+				</p>
+			</div>
+		</div>
+		<div class="accordion">
+			<div class="accordion__head">
+				Документы необходимые для подачи заявления о страховой выплате:
+			</div>
+			<div class="accordion__body accordion__body__2-column">
+				<div class="row">
+					<div class="column">
+						<ul>
+							<li>
+								<p>
+									Обязательные и документы о собственности:
+								</p>
+								<ol>
+									<li>
+										заявление о страховом событии;
+									</li>
+									<li>
+										страховой полис / договор комплексного страхования;
+									</li>
+									<li>
+										квитанция об оплате Договора страхования;
+									</li>
+									<li>
+										документ, удостоверяющий личность (паспорт);
+									</li>
+									<li>
+										свидетельство о регистрации автомобиля;
+									</li>
+									<li>
+										паспорт транспортного средства (ПТС);
+									</li>
+									<li>
+										талон прохождения ГТО;
+									</li>
+									<li>
+										водительское удостоверение лица, управлявшего ТС на момент ДТП;
+									</li>
+									<li>
+										реквизиты банка для перечисления страхового возмещения.
+									</li>
+								</ol>
+							</li>
+						</ul>
+						<ul>
+							<li>В случае ДТП к ним прилагаются (при наличии):</li>
+						</ul>
+						<ul>
+							<li>
+								<p>
+									В случае противоправных действий третьих лиц (ПДТЛ):
+								</p>
+								<ol>
+									<li>
+										талон-уведомление УВД;
+									</li>
+									<li>
+										постановление о возбуждении/отказе/закрытии возбуждении уголовного дела;
+									</li>
+									<li>
+										справка ОВД (форма №3).
+									</li>
+								</ol>
+							</li>
+						</ul>
+						<ul>
+							<li>
+								<p>
+									Документы, подтверждающие расходы страхователя:
+								</p>
+								<ol>
+									<li>
+										заказ-наряд СТОА с печатью организации;
+									</li>
+									<li>
+										кассовый чек / акт выполненных работ с подписью страхователя о том,  что работы выполнены полностью и замечаний по качеству ремонта нет;
+									</li>
+									<li>
+										затраты по оплате услуг эвакуатора.
+									</li>
+								</ol>
+							</li>
+						</ul> 
+					</div>
+					<div class="column">
+						<ul>
+							<li>
+								<p>Иное:</p>
+								<ol>
+									<li>
+										документы, подтверждающие оказание и оплату услуг по хранению повреждённого имущества, если потерпевший требует возмещения этих расходов;
+									</li>
+									<li>
+										иные документы, необходимые для обоснования требования о возмещении причиненного вреда, в том числе сметы и счета, подтверждающие стоимость ремонта повреждённого имущества. Это могут быть оригиналы документов либо их копии, заверенные нотариально, или выдавшим документы лицом (органом), или страховщиком. На основании этих документов страховая компания должна:
+									</li>
+									<li>
+										Согласовав время и место, в течение пяти рабочих дней (со дня подачи всех указанных выше документов и заявления на страховую выплату) произвести независимую экспертизу в присутствии участников ДТП и представителя страховой компании. Когда характер повреждений или особенности повреждённого имущества исключают представление ТС для осмотра по месту нахождения эксперта (например, машина не в состоянии двигаться), экспертизу проводят по месту нахождения этого имущества.
+									</li>
+									<li>
+										Составить акт о страховом случае не позже 20 календарных дней со дня получения всех необходимых из указанных выше документов и принять решение о страховой выплате.
+									</li>
+									<li>
+										Известить потерпевшего о возмещении ущерба на основании акта о страховом случае, о полном или частичном отказе, указав причины.
+									</li>
+									<li>
+										Если страховая выплата, отказ в ней или изменение её размера зависят от результатов уголовного, гражданского дела либо дела об административном правонарушении, срок выплаты может быть продлен до их закрытия или окончания и вступления в силу решения суда.
+									</li>
+									<li>
+										Если между страховщиком и потерпевшим есть разногласия относительно размера компенсации, страховая компания в любом случае обязана выплатить неоспариваемую ею часть. Далее претендент на страховую выплату может подать в суд на страховую компанию на предмет выплаты ему дополнительной суммы.
+									</li>
+								</ol>
+							</li>
+						</ul> 
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="accordion">
+			<div class="accordion__head">
+				Контактные телефоны страховых компаний
+			</div>
+			<div class="accordion__body">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic laboriosam recusandae vel cupiditate at voluptatum, aliquid dolor sit, expedita consequuntur perferendis. Error, laborum inventore necessitatibus maiores odio harum mollitia quod ullam eveniet non, quasi modi labore in similique nostrum eum laboriosam delectus? Veniam temporibus, itaque officia impedit autem iste aspernatur.
+				</p>
+			</div>
+		</div>
+		<div class="accordion">
+			<div class="accordion__head">
+				Максимальная сумма страховых выплат по ОСАГО
+			</div>
+			<div class="accordion__body">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic laboriosam recusandae vel cupiditate at voluptatum, aliquid dolor sit, expedita consequuntur perferendis. Error, laborum inventore necessitatibus maiores odio harum mollitia quod ullam eveniet non, quasi modi labore in similique nostrum eum laboriosam delectus? Veniam temporibus, itaque officia impedit autem iste aspernatur.
+				</p>
+			</div>
+		</div>
+		<div class="accordion">
+			<div class="accordion__head">
+				Документы для оформления электронного полиса ОСАГО
+			</div>
+			<div class="accordion__body">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic laboriosam recusandae vel cupiditate at voluptatum, aliquid dolor sit, expedita consequuntur perferendis. Error, laborum inventore necessitatibus maiores odio harum mollitia quod ullam eveniet non, quasi modi labore in similique nostrum eum laboriosam delectus? Veniam temporibus, itaque officia impedit autem iste aspernatur.
+				</p>
+			</div>
+		</div>
+		<div class="accordion">
+			<div class="accordion__head">
+				Проверка коэффициента бонус-малус (КБМ)  
+			</div>
+			<div class="accordion__body">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic laboriosam recusandae vel cupiditate at voluptatum, aliquid dolor sit, expedita consequuntur perferendis. Error, laborum inventore necessitatibus maiores odio harum mollitia quod ullam eveniet non, quasi modi labore in similique nostrum eum laboriosam delectus? Veniam temporibus, itaque officia impedit autem iste aspernatur.
+				</p>
+			</div>
+		</div>
+		<div class="accordion">
+			<div class="accordion__head">
+				Проверка подлинности электронного полиса ОСАГО
+			</div>
+			<div class="accordion__body">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic laboriosam recusandae vel cupiditate at voluptatum, aliquid dolor sit, expedita consequuntur perferendis. Error, laborum inventore necessitatibus maiores odio harum mollitia quod ullam eveniet non, quasi modi labore in similique nostrum eum laboriosam delectus? Veniam temporibus, itaque officia impedit autem iste aspernatur.
+				</p>
+			</div>
+		</div>
+		<div class="accordion">
+			<div class="accordion__head">
+				Причины отказа в выплате страхового вознаграждения
+			</div>
+			<div class="accordion__body">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic laboriosam recusandae vel cupiditate at voluptatum, aliquid dolor sit, expedita consequuntur perferendis. Error, laborum inventore necessitatibus maiores odio harum mollitia quod ullam eveniet non, quasi modi labore in similique nostrum eum laboriosam delectus? Veniam temporibus, itaque officia impedit autem iste aspernatur.
+				</p>
+			</div>
+		</div>
+		<div class="accordion">
+			<div class="accordion__head">
+				Страховая история, скидка за безаварийное вождение (коэффициент бонус-малус — КБМ) при оформлении электронного ОСАГО
+			</div>
+			<div class="accordion__body">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic laboriosam recusandae vel cupiditate at voluptatum, aliquid dolor sit, expedita consequuntur perferendis. Error, laborum inventore necessitatibus maiores odio harum mollitia quod ullam eveniet non, quasi modi labore in similique nostrum eum laboriosam delectus? Veniam temporibus, itaque officia impedit autem iste aspernatur.
+				</p>
+			</div>
+		</div>
+		<div class="accordion">
+			<div class="accordion__head">
+				Расторжение договора ОСАГО, оформленного в электронном виде
+			</div>
+			<div class="accordion__body">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic laboriosam recusandae vel cupiditate at voluptatum, aliquid dolor sit, expedita consequuntur perferendis. Error, laborum inventore necessitatibus maiores odio harum mollitia quod ullam eveniet non, quasi modi labore in similique nostrum eum laboriosam delectus? Veniam temporibus, itaque officia impedit autem iste aspernatur.
+				</p>
+			</div>
+		</div>
+	</div>
+</section> 
