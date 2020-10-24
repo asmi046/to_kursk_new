3755bfdee90ccde8d04a4f2929548035e397fee0
ruvick <rudikov.web@ya.ru>
2020-10-24 18:51:24 +0300

    Окно поправил + отзывы

--- modal-win.php
+++ modal-win.php
@@ -1,5 +1,187 @@
-
 <div style="display: none;">
+    <div class="box-modal" id="popupTO">
+      <div class="box-modal_close arcticmodal-close"><?_e("","rubex");?></div>
+    	
+      	<div class="popup__title">Выберите свой город</div>
+				<div class="popup__menu">
+
+					<nav class="popup__column">
+						<ul class="popup__column-list">
+							<li class="popup__letter">А</li>
+							<li><a href="#">Адыгейск</a></li>
+							<li><a href="#">Алейск</a></li>
+							<li><a href="#">Ахара</a></li>
+							<li><a href="#">Архангельск</a></li>
+							<li><a href="#">Астрахань</a></li>
+						</ul>
+ 
+						<ul class="popup__column-list">
+							<li class="popup__letter">Б</li>
+							<li><a href="#">Белорецк</a></li>
+							<li><a href="#">Белгород</a></li>
+							<li><a href="#">Брянск</a></li>
+							<li><a href="#">Багдарин</a></li>
+						</ul>
+
+					<ul class="popup__column-list">
+							<li class="popup__letter">В</li>
+							<li><a href="#">Владимир</a></li>
+							<li><a href="#">Волгоград</a></li>
+							<li><a href="#">Вологодск</a></li>
+							<li><a href="#">Воронеж</a></li>
+						</ul>
+
+					<ul class="popup__column-list">
+							<li class="popup__letter">Д</li>
+							<li><a href="#">Дербент</a></li>
+						</ul>
+
+					<!-- <ul class="popup__column-list">
+							<li class="popup__letter">Е</li>
+							<li><a href="#">Еврейская АО</a></li>
+						</ul> -->
+					</nav>
+
+					<nav class="popup__column">
+						<ul class="popup__column-list">
+							<li class="popup__letter">З</li>
+							<li><a href="#">Забайкальск</a></li>
+						</ul>
+
+						<ul class="popup__column-list">
+							<li class="popup__letter">И</li>
+							<li><a href="#">Иваново</a></li>
+							<li><a href="#">Исконтне</a></li>
+							<li><a href="#">Иркутск</a></li>
+						</ul>
+
+						<ul class="popup__column-list">
+							<li class="popup__letter">К</li>
+							<li><a href="#">Кенже</a></li>
+							<li><a href="#">Калининград</a></li>
+							<li><a href="#">Кевюды</a></li>
+							<li><a href="#">Калуга</a></li>
+							<li><a href="#">Карымай</a></li>
+							<li><a href="#">Карачаевск</a></li>
+							<li><a href="#">Кемь</a></li>
+							<li><a href="#">Кемерово</a></li>
+							<li><a href="#">Киров</a></li>
+							<li><a href="#">Кожым</a></li>
+							<li><a href="#">Кострома</a></li>
+							<li><a href="#">Краснодар</a></li>
+							<li><a href="#">Красноярск</a></li>
+							<li><a href="#">Керчь</a></li>
+							<li><a href="#">Курган</a></li>
+							<li><a href="#">Курск</a></li>
+						</ul>
+					</nav>
+
+					<nav class="popup__column">
+						<ul class="popup__column-list">
+							<li class="popup__letter">Л</li>
+							<li><a href="#">Луга</a></li>
+							<li><a href="#">Липецк</a></li>
+						</ul>
+
+						<ul class="popup__column-list">
+							<li class="popup__letter">М</li>
+							<li><a href="#">Магадан</a></li>
+							<li><a href="#">Морки</a></li>
+							<!-- <li><a href="#">Мордовия</a></li> -->
+							<li><a href="#">Москва</a></li>
+							<li><a href="#">Мурманск</a></li>
+						</ul>
+
+					<ul class="popup__column-list">
+							<li class="popup__letter">Н</li>
+							<li><a href="#">Нельмин-Нос</a></li>
+							<li><a href="#">Нижний Новгород</a></li>
+							<!-- <li><a href="#">Новгородская область</a></li> -->
+							<li><a href="#">Новосибирск</a></li>
+						</ul>
+
+					<ul class="popup__column-list">
+							<li class="popup__letter">О</li>
+							<li><a href="#">Омск</a></li>
+							<li><a href="#">Оренбург</a></li>
+							<li><a href="#">Орел</a></li>
+						</ul>
+
+					<ul class="popup__column-list">
+							<li class="popup__letter">П</li>
+							<li><a href="#">Пенза</a></li>
+							<li><a href="#">Пермь</a></li>
+							<li><a href="#">Партизанск</a></li>
+							<li><a href="#">Псков</a></li>
+						</ul>
+					</nav>
+
+					<nav class="popup__column">
+						<ul class="popup__column-list">
+							<li class="popup__letter">Р</li>
+							<li><a href="#">Ростов</a></li>
+							<li><a href="#">Рязань</a></li>
+						</ul>
+
+						<ul class="popup__column-list">
+							<li class="popup__letter">С</li>
+							<li><a href="#">Самара</a></li>
+							<li><a href="#">Саратов</a></li>
+							<li><a href="#">Среднеколымск</a></li>
+							<li><a href="#">Северо-Курильск</a></li>
+							<li><a href="#">Свердловск</a></li>
+							<li><a href="#">Сунжа</a></li>
+							<li><a href="#">Смоленск</a></li>
+							<li><a href="#">Ставрополь</a></li>
+						</ul>
+
+					<ul class="popup__column-list">
+							<li class="popup__letter">Т</li>
+							<li><a href="#">Тамбов</a></li>
+							<li><a href="#">Тетюши</a></li>
+							<li><a href="#">Тверь</a></li>
+							<li><a href="#">Томск</a></li>
+							<li><a href="#">Тула</a></li>
+							<li><a href="#">Туран</a></li>
+							<li><a href="#">Тюмень</a></li>
+						</ul>
+
+					<ul class="popup__column-list">
+							<li class="popup__letter">У</li>
+							<!-- <li><a href="#">Удмуртия</a></li> -->
+							<li><a href="#">Ульяновск</a></li>
+						</ul>
+					</nav>
+
+					<nav class="popup__column">
+						<ul class="popup__column-list">
+							<li class="popup__letter">Х</li>
+							<li><li><a href="#">Ханкуль</a></li></li>
+							<li><li><a href="#">Ханты-Мансийск</a></li></li>
+						</ul>
+
+						<ul class="popup__column-list">
+							<li class="popup__letter">Ч</li>
+							<li><a href="#">Челябинск</a></li>
+							<li><a href="#">Чири-Юрт</a></li>
+							<li><a href="#">Чебоксары</a></li>
+						</ul>
+
+						<ul class="popup__column-list">
+							<li class="popup__letter">Я</li>
+							<li><a href="#">Ямбург</a></li>
+							<li><a href="#">Ярославль</a></li>
+						</ul>
+					</nav>
+
+				</div>
+
+    </div>
+</div>
+
+
+
+<!-- <div style="display: none;">
     <div class="box-modal" id="popupTO">
       <div class="box-modal_close arcticmodal-close"><?_e("закрыть","rubex");?></div>
     	
@@ -179,9 +361,9 @@
 				</div>
 
     </div>
-</div>
+</div> -->
 
-<div style="display: none;">
+<!-- <div style="display: none;">
     <div class="box-modal" id="popupOS">
       <div class="box-modal_close arcticmodal-close"><?_e("закрыть","rubex");?></div>
     	
@@ -361,4 +543,4 @@
 				</div>
 
     </div>
-</div>
+</div> -->
---
+++ template-parts/reviews-section.php
@@ -0,0 +1,109 @@
+<section class="reviews">
+	<div class="inner">
+		<div class="reviews__body">
+			<div class="reviews__title">
+				<h1>Отзывы наших клиентов</h1>
+			</div>
+			<div class="reviews__card">
+				<div class="reviews__image">
+					<img src="<?php echo get_template_directory_uri();?>/img/reviews.jpg" class="spacer" alt="">
+				</div>
+				<div class="reviews__content">
+					<div class="reviews__title-cont">
+						<h2>Иванов Иван Иванович</h2>
+					</div>
+					<div class="reviews__subtitle">
+						<p>
+							Lorem ipsum, dolor sit amet, consectetur adipisicing elit. 
+							Obcaecati magni itaque nisi reiciendis, tempore, distinctio 
+							quam vitae, a iste similique eius, veritatis error sit delectus velit atque at magnam. 
+							Veritatis. Consequuntur, libero. Debitis, eveniet, sunt. Unde delectus adipisci a, veniam 
+							laborum voluptas doloremque ullam in quod dignissimos ipsa eaque quidem sapiente praesentium 
+							quis repellat possimus non natus voluptatum dolores maxime.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="reviews__card">
+				<div class="reviews__image">
+					<img src="<?php echo get_template_directory_uri();?>/img/reviews.jpg" class="spacer" alt="">
+				</div>
+				<div class="reviews__content">
+					<div class="reviews__title-cont">
+						<h2>Иванов Иван Иванович</h2>
+					</div>
+					<div class="reviews__subtitle">
+						<p>
+							Lorem ipsum, dolor sit amet, consectetur adipisicing elit. 
+							Obcaecati magni itaque nisi reiciendis, tempore, distinctio 
+							quam vitae, a iste similique eius, veritatis error sit delectus velit atque at magnam. 
+							Veritatis. Consequuntur, libero. Debitis, eveniet, sunt. Unde delectus adipisci a, veniam 
+							laborum voluptas doloremque ullam in quod dignissimos ipsa eaque quidem sapiente praesentium 
+							quis repellat possimus non natus voluptatum dolores maxime.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="reviews__card">
+				<div class="reviews__image">
+					<img src="<?php echo get_template_directory_uri();?>/img/reviews.jpg" class="spacer" alt="">
+				</div>
+				<div class="reviews__content">
+					<div class="reviews__title-cont">
+						<h2>Иванов Иван Иванович</h2>
+					</div>
+					<div class="reviews__subtitle">
+						<p>
+							Lorem ipsum, dolor sit amet, consectetur adipisicing elit. 
+							Obcaecati magni itaque nisi reiciendis, tempore, distinctio 
+							quam vitae, a iste similique eius, veritatis error sit delectus velit atque at magnam. 
+							Veritatis. Consequuntur, libero. Debitis, eveniet, sunt. Unde delectus adipisci a, veniam 
+							laborum voluptas doloremque ullam in quod dignissimos ipsa eaque quidem sapiente praesentium 
+							quis repellat possimus non natus voluptatum dolores maxime.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="reviews__card">
+				<div class="reviews__image">
+					<img src="<?php echo get_template_directory_uri();?>/img/reviews.jpg" class="spacer" alt="">
+				</div>
+				<div class="reviews__content">
+					<div class="reviews__title-cont">
+						<h2>Иванов Иван Иванович</h2>
+					</div>
+					<div class="reviews__subtitle">
+						<p>
+							Lorem ipsum, dolor sit amet, consectetur adipisicing elit. 
+							Obcaecati magni itaque nisi reiciendis, tempore, distinctio 
+							quam vitae, a iste similique eius, veritatis error sit delectus velit atque at magnam. 
+							Veritatis. Consequuntur, libero. Debitis, eveniet, sunt. Unde delectus adipisci a, veniam 
+							laborum voluptas doloremque ullam in quod dignissimos ipsa eaque quidem sapiente praesentium 
+							quis repellat possimus non natus voluptatum dolores maxime.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="reviews__card">
+				<div class="reviews__image">
+					<img src="<?php echo get_template_directory_uri();?>/img/reviews.jpg" class="spacer" alt="">
+				</div>
+				<div class="reviews__content">
+					<div class="reviews__title-cont">
+						<h2>Иванов Иван Иванович</h2>
+					</div>
+					<div class="reviews__subtitle">
+						<p>
+							Lorem ipsum, dolor sit amet, consectetur adipisicing elit. 
+							Obcaecati magni itaque nisi reiciendis, tempore, distinctio 
+							quam vitae, a iste similique eius, veritatis error sit delectus velit atque at magnam. 
+							Veritatis. Consequuntur, libero. Debitis, eveniet, sunt. Unde delectus adipisci a, veniam 
+							laborum voluptas doloremque ullam in quod dignissimos ipsa eaque quidem sapiente praesentium 
+							quis repellat possimus non natus voluptatum dolores maxime.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
