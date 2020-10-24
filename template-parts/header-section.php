4e87e2a19ae78163fa7db048b9f1592b9bb0c61a
ruvick <rudikov.web@ya.ru>
2020-10-24 21:17:47 +0300

    Окно выбор города

--- template-parts/header-section.php
+++ template-parts/header-section.php
@@ -1,27 +1,26 @@
 <header id="header" class="header lock-padding">
-            <div class="header__top">
-                <div class="inner">
-                    <a href="<?echo get_home_url()?>" class="main-logo header__top__item">
-                        <img src="<?php echo get_template_directory_uri();?>/img/main-logo.svg" class="spacer" alt="">
-                    </a>
-                    <a href="#" class="burger-btn">
-                        <span></span>
-                    </a>
-                    <div class="header__top__item work-time">
-                        Работаем <span>24</span>/<span>7</span>
-                    </div>
-                    <a href="#" class="header__top__item location">
-                        <span class="marker"></span>Курск
-                    </a>
-                    <div class="header__top__item contact-box">
-                        <span class="db desktop phone"><? echo $tel = carbon_get_theme_option("to_phone"); ?></span>
-                        <a href="tel:<? echo preg_replace('/[^0-9]/', '', $tel); ?>" class="db mobil phone"><? echo $tel; ?></a>
-                        <a href="mailto:<? echo $mail = carbon_get_theme_option("to_mail"); ?>" class="email"><? echo $mail; ?></a>
-                    </div>
-                </div>
+    <div class="header__top">
+        <div class="inner">
+            <a href="<?echo get_home_url()?>" class="main-logo header__top__item">
+                <img src="<?php echo get_template_directory_uri();?>/img/main-logo.svg" class="spacer" alt="">
+            </a>
+            <a href="#" class="burger-btn">
+                <span></span>
+            </a>
+            <div class="header__top__item work-time">
+                Работаем <span>24</span>/<span>7</span>
             </div>
-            <?php get_template_part('template-parts/popups');?>
-            <?php get_template_part('template-parts/menu-button');?>
-            
-            
-        </header>
+            <a href="#" class="header__top__item location">
+                <span class="marker"></span>Курск
+            </a>
+            <div class="header__top__item contact-box">
+                <span class="db desktop phone"><? echo $tel = carbon_get_theme_option("to_phone"); ?></span>
+                <a href="tel:<? echo preg_replace('/[^0-9]/', '', $tel); ?>" class="db mobil phone"><? echo $tel; ?></a>
+                <a href="mailto:<? echo $mail = carbon_get_theme_option("to_mail"); ?>" class="email"><? echo $mail; ?></a>
+            </div>
+        </div>
+    </div>
+    <?php get_template_part('template-parts/popups');?>
+    <?php get_template_part('template-parts/popup-gorod');?>
+    <?php get_template_part('template-parts/menu-button');?>
+</header>
---
+++ template-parts/popup-gorod.php
@@ -0,0 +1,7 @@
+<div class="city_vsp_vin" style="display:none;">
+	<div class="qq">Ваш город <span style="city_in_win">Курск</span>?</div>
+	<div class="qq_btn">
+		<button class="yes_no_btn yes_btn btn btn-pink">Да, спасибо</button>
+		<button class="yes_no_btn no_btn btn btn-pink">Нет, другой</button>
+	</div>
+</div>
