798ff808ac5f3591f188afe39ad567a23ce31653
asmi046 <asmi046@gmail.com>
2020-10-26 18:46:56 +0300

    Метрика

--- header.php
+++ header.php
@@ -28,7 +28,23 @@
 </head>
    
 <body>
-    
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(68712259, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true,
+            ecommerce:"dataLayer"
+    });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/68712259" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
+
     <? include "modal-win.php";?>
     <div class="great-shadow"></div>
 
