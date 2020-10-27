1abf351569c4d01d9df324efbc660e3cdf25ff17
ruvick <rudikov.web@ya.ru>
2020-10-28 01:06:33 +0300

    Страницы: Контакты, Деятельность, Способы оплаты

---
+++ template-parts/requisites-section.php
@@ -0,0 +1,23 @@
+<section class="requisites">
+	<div class="inner">
+		<div class="requisites__body">
+			<div class="requisites__title">
+				<h1>Контакты и реквизиты компании</h1>
+			</div>
+			<div class="requisites__under-title">
+				<h2>ИП ЗАХАРОВ ВИТАЛИЙ НИКОЛАЕВИЧ</h2>
+			</div>
+			<div class="requisites__subtitle">
+				<p>
+					Юридический адрес:  г. Курск, ул. Гунатовская, д. 32 <br>
+					ИНН/КПП:  463239054056/0 <br>
+					Р/С:  40802810233000006083 <br>
+					в Отделение № 8596 Сбербанка России <br>
+					К/С:  30101810300000000606 <br>
+					БИК:  043807606
+				</p>
+				<p><a href="tel:+78002221191">8 (800) 222 11 91</a></p>
+			</div>
+		</div>
+	</div>
+</section>
